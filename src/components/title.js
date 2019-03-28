0f2b32ef5ad0f5cc497d73a9a1c962d7b5acd22f
unknown <russel.tsang@gmail.com>
2019-03-28 00:03:28 -0400

    added display text

--- src/components/title.js
+++ src/components/title.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Title = (props) => {
     return (
         <div>
-            <h1>Rain || Shine</h1>
+            <h1>Rain || Shine ?</h1>
+            <p>Enter your location to find out...</p>
         </div>
     )
 }
